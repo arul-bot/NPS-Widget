d6f9ffd947adbc52fefbe1e9ece87aa48cc8d62e
AruL <arulnik07@gmail.com>
2022-11-11 14:26:05 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+
+	],
 })
-export class AppComponent {
+export class AppComponent{
+
   title = 'Bootstrap_nps';
+  input! : boolean;
+  ctrl = new FormControl<number | null>(null, Validators.required);
+
+  toggle() {
+		if (this.ctrl.value! <= 7) {
+			this.input = true;
+		}else{
+      this.input = false;
+    }
+	}
+  OnSubmit(){
+    if(this.ctrl.value! <= 7){
+      this.toggle();
+    }else{
+      this.AfterSubmit();
+      this.toggle();
+    }
+  }
+
+  AfterSubmit(){
+    console.warn(this.ctrl.value);
+    console.warn(this.input);
+    
+  }
+  
 }
